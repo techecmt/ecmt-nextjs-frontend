ee32cdf7f40565bc8daf18e7317b48cc4dd70f19
Riyas Al Mohamed <riyas.hs2021@gmail.com>
2026-05-27 16:33:18 +0530

    ADME update

--- app/advanced-diploma-in-mechanical-and-electrical-engineering/page.tsx
+++ app/advanced-diploma-in-mechanical-and-electrical-engineering/page.tsx
@@ -219,13 +219,14 @@ export default function AdvancedDiplomaInMechanicalAndElectricalEngineering() {
                                     <svg className="w-5 h-5 text-[#EE4A62] mt-0.5 shrink-0" fill="currentColor" viewBox="0 0 20 20">
                                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                                     </svg>
-                                    <span>English language proficiency: GCE 'A' level or equivalent</span>
+                                    <span>English language proficiency: GCE ‘A’ level or equivalent</span>
                                 </li>
                                 <li className="flex items-start gap-3">
                                     <svg className="w-5 h-5 text-[#EE4A62] mt-0.5 shrink-0" fill="currentColor" viewBox="0 0 20 20">
                                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                                     </svg>
-                                    <span>Working Experience: Matured candidate (i.e. minimum 30 years old with 8 years of working experience) with other relevant certificate qualification with 3 years' relevant work experience</span>
+                                    <span>Working Experience: Matured candidate with other relevant certificate qualification with 3
+                                    years' relevant work experience.</span>
                                 </li>
                             </ul>
                         </div>
